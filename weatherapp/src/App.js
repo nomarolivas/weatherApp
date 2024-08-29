9d48d80f4674a281914deff4c5c93243990b98f4
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-08-28 20:43:09 -0700

    Added HTML and API File

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,15 +2,38 @@ import React, {useState} from "react";
 import axios from 'axios'
 
 
-function App() {
+  function App() {
 
-  const url= 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+  //const url= 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5e73cb2a276e2ee3fae5b79f06a8c105'
 
 
   return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+              <p>Dallas</p>
+            </div>
+            <div className="temp">
+              <h1>69°F</h1>
+            </div>
+            <div className="description">
+              <p>Clouds</p>
+            </div>
+          </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
 
-    <div className="App">
-<h1>Hello</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
